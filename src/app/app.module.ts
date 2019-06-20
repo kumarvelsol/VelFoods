0faeabb5cc9ee472e44400aab92ee0ffebc4ca89
GuduruUmesh <35020243+GuduruUmesh@users.noreply.github.com>
2019-06-20 10:05:44 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatTableModule} from '@angular/material/table';
 import { MatRadioModule} from '@angular/material/radio';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatSelectModule} from '@angular/material/select';
 import { CommonModule } from '@angular/common';
-import { MatCardModule} from '@angular/material/card'
-import { MatIconModule} from '@angular/material/icon';
 import { FormsModule,ReactiveFormsModule }   from '@angular/forms';
-import { MatInputModule} from '@angular/material/input';
-import { MatFormFieldModule} from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -19,7 +13,7 @@ import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { ItemCategoryComponent } from './item-category/item-category.component';
 import { ItemNamesComponent } from './item-names/item-names.component';
 import { RestaurantModule } from './restaurant/restaurant.module';
-
+import { MaterialModule } from './shared/material.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,13 +23,12 @@ import { RestaurantModule } from './restaurant/restaurant.module';
     ItemNamesComponent
   ],
   imports: [
+    MaterialModule,
     BrowserModule,AppRoutingModule,
     ReactiveFormsModule,FormsModule,
     BrowserAnimationsModule,RestaurantModule,
-    MatFormFieldModule,MatInputModule,
-    MatIconModule,MatCardModule,
-    MatTableModule,CoreModule,
-    CommonModule,MatSelectModule,
+    CoreModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
