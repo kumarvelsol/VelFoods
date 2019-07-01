7ab23ff4d5cd0a859cd6cda9b561b9431b54c2f7
GuduruUmesh <35020243+GuduruUmesh@users.noreply.github.com>
2019-07-01 16:43:23 +0530

    changes

--- src/app/ordering/ordering.component.ts
+++ src/app/ordering/ordering.component.ts
@@ -1,59 +1,92 @@
 import { Component, OnInit } from '@angular/core';
-import { ServiceService } from '../service.service';
+import {ServiceService} from '../service.service';
 import { order, room } from '../Model/ordermodel';
+import { NgStyle } from '@angular/common';
+import { style } from '@angular/animations';
+import { Button } from 'protractor';
+import { templateJitUrl } from '@angular/compiler';
 
 @Component({
-  selector: 'app-ordering',
-  templateUrl: './ordering.component.html',
-  providers:[ServiceService],
-  styleUrls: ['./ordering.component.css'],
- })
+ selector: 'app-ordering',
+ templateUrl: './ordering.component.html',
+ providers:[ServiceService],
+ styleUrls: ['./ordering.component.css'],
+})
 export class OrderingComponent implements OnInit {
   userlist:order;
   datasource;
   rooms : room[];
-  name : string;
-  roomnos : string;
-  colors : string;
-  BACKGROUND_COLOR : string;
-  colorrs : string;
-  orn : 'orange';
-  redd : 'red';
-  colorr : any = {};
+  name:string;
+  roomnos :string;
+  colors :string;
+  BACKGROUND_COLOR:string;
+  colorrs : 'green';
+  orn :'orange';
+  redd :'red';
+  colorr:any = {};
   //mystyles :string;
-  colorFlag: any;
+ colorFlag: any;
   //rooms :any ={};
-  constructor(private _roomservice : ServiceService) {
+  ROOM_NO : any ={};
+ constructor(private _roomservice : ServiceService) {
   }
-  ngOnInit() {
-    this._roomservice.roomno().subscribe((data : order) =>
-    {
-      this.userlist = data;
-      this.rooms = this.userlist.Data;
-      for(let i=0; i<this.rooms.length ; i++){
-        if(this.rooms[i].BACKGROUND_COLOR == "Green")
-        {
-          this.colorr[i] ='#00F300';
-        }
-        else if(this.rooms[i].BACKGROUND_COLOR == "orange")
-        {
-          this.colorr[i] ='#F39000';
-        }
-        else if(this.rooms[i].BACKGROUND_COLOR =="Red")
-        {
-          this.colorr[i] ='#F30000';
-        }
+
+ ngOnInit() {
+ debugger;
+   this._roomservice.roomno().subscribe((data : order) =>
+   {
+     this.userlist=data;
+     this.rooms = this.userlist.Data;
+     for(let i=0; i<this.rooms.length ; i++){
+       if(this.rooms[i].BACKGROUND_COLOR == "Green")
+       {
+
+         // this.colorr = this.rooms[i].ROOM_NO.toString();
+         // //this.colorr ='#FF5733';
+         // return this.colorr = NgStyle [ 'green']
+       }
+       if(this.rooms[i].BACKGROUND_COLOR == "Orange")
+       {
+        // alert('orange');
+       }
+       if(this.rooms[i].BACKGROUND_COLOR =="Red")
+       {
+         //this.colors =this.rooms[i].ROOM_NO.toString();
+         //this.colorr ='#FF5737';
+        // this.colors =this.colorFlag? 'back-ground' :'green';
+        // alert('red');
+       }
       }
-      console.log(this.colorr);
-      console.log(this.colorrs);
-      console.log(this.userlist.Data);
-    });
-  }
-  onbuttonclick($event,ROOM_NO){
-    this.colorr;
-    alert(ROOM_NO);
-  }
-  getmystyles()
-  {
-  }
+
+     console.log(this.userlist.Data);
+   });
+   this.getmystyles(this.aa);
+ }
+ onbuttonclick($event,ROOM_NO){
+   this.colorr ;
+   alert(ROOM_NO);
+ }
+
+ public  aa : string ;
+getmystyles(aa)
+{
+//   this._roomservice.roomno().subscribe((data : order) =>
+//   {
+//   this.userlist=data;
+//   this.rooms = this.userlist.Data;
+//   for(let i=0; i<this.rooms.length ; i++){
+//   var s =  this.rooms[i].ROOM_NO.toString()
+//     switch(aa){
+
+//      case this.rooms[i].BACKGROUND_COLOR ='Green':
+//        return s ='green';
+//        case this.rooms[i].BACKGROUND_COLOR ='orange':
+//           return s ='orage';
+//           case this.rooms[i].BACKGROUND_COLOR ='Red':
+//               return s ='red';
+//     }
+//   }
+//   console.log(this.userlist.Data);
+//     });
+ }
 }
