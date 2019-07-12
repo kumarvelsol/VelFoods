a827698bb53d0fd56b215696e3e8ee9abbe254ff
Aneesha <aneeshap.velsol@gmail.com>
2019-07-12 12:11:32 +0530

    changes

--- src/app/restaurant/restaurant.service.ts
+++ src/app/restaurant/restaurant.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpParams, HttpClient } from '@angular/common/http';
+import { Property } from '../shared/property';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantService {
+  Baseurl = 'http://localhost:57649/';
+  constructor(private http:HttpClient) { }
+  
 
-  constructor() { }
+  public getproperty(prpt_get : Property)
+  {
+    return this.http.post(this.Baseurl+"getproperty",prpt_get);
+  }
+  public createproperty(prpt_in:Property){
+      return this.http.post(`${this.Baseurl+"addingproperty"}`,prpt_in);
+  }
+  public updateproperty(prpt_up: Property){
+    return this.http.post(`${this.Baseurl+"updatingproperty"}`,prpt_up);
+  }
 }
---
+++ src/app/shared/property.ts
@@ -0,0 +1,14 @@
+export interface Property {
+    property_name : string;
+    property_address : string;
+    property_mobile_no : number;
+    property_land_mark : string;
+    property_landline : number;
+    property_city : string;
+    property_email : string;
+    property_state : string;
+    property_website : string;
+    property_pincode : number;
+    property_gst : string;
+    property_country : string;                    
+}
