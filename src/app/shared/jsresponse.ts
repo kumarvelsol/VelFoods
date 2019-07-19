1b43f4b5d168338374e83cf3742a6bcdd813ae68
SRIKARTHATI <srikar.velsol@gmail.com>
2019-07-19 12:24:35 +0530

    changes

--- src/app/shared/jsresponse.ts
+++ src/app/shared/jsresponse.ts
@@ -24,6 +24,28 @@ export interface Data {
     empregistration_login_type:string;
     Username:string;
     password:string;
+    itemcategory_id:number;
+    item_name:string;
+    item_description:string;
+    item_active_from:Date;
+    item_status:string;
+    item_reporting_name:string;
+    itemname_id:number;
+    itemname_item_name:string;
+    itemname_description:string;
+    itemname_reportingname:string;
+    itemname_active_from:Date;
+    itemname_status:string;
+    item_dinein_amount:number;
+    item_dinein_tax:number;
+    item_takeaway_amount:number;
+    item_takeaway_tax:number;
+    item_homedelivary_amount:number;
+    item_homedelivary_tax:number;
+    item_homedelivery_deliverycharges:number;
+    itemname_dinein_total:string;
+    itemname_takeaway_total:string;
+    itemname_homedelivary_total:string;
     tax_id : number;
     tax_name : string;
     tax_percentage : number;
@@ -48,13 +70,4 @@ export interface Data {
     minbill_amount : string;
     maximum_bill_status : string;
     maximum_bill_amount : string;
-    table_defination_id : number;
-    table_capatain : string;
-    table_description: string;
-    table_name: string;
-    table_pax: number;
-    table_status: string;
-    table_steward: string;
-    table_view : string;
-    
 }
