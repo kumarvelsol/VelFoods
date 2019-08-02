c8606d5ad063349ac71a058518597fb4153a0d56
Dharani <lakshmim.velsol@gmail.com>
2019-08-02 14:28:32 +0530

    Changes

---
+++ src/app/shared/interfaces/managermodel.ts
@@ -0,0 +1,9 @@
+export interface Managermodel {
+    manger_id:number;
+    manger_name:string;
+    manger_mobile_no:string;
+    manger_id_proof:string;
+    manger_id_no:string;
+    manger_status:string
+    restaurent_id:number;
+}
