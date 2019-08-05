f66341177a6acc9cfaa7cec84c37aaf752fd2e95
Aneesha <aneeshap.velsol@gmail.com>
2019-08-05 15:15:07 +0530

    changes

--- src/app/shared/jsresponse.ts
+++ src/app/shared/jsresponse.ts
@@ -70,17 +70,5 @@ export interface Data {
     minbill_amount : string;
     maximum_bill_status : string;
     maximum_bill_amount : string;
-    property_id : number;
-    property_name : string;
-    property_address : string;
-    property_mobile_no : number;
-    property_land_mark : string;
-    property_landline : number;
-    property_city : string;
-    property_email : string;
-    property_state : string;
-    property_website : string;
-    property_pincode : number;
-    property_gst : string;
-    property_country : string;     
+    
 }
