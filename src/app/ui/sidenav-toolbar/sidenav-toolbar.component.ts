5bbe12fb9622ea6ce28bd35bdd5ea6861236790c
GuduruUmesh <35020243+GuduruUmesh@users.noreply.github.com>
2019-08-20 15:32:58 +0530

    changes

---
+++ src/app/ui/sidenav-toolbar/sidenav-toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav-toolbar',
+  templateUrl: './sidenav-toolbar.component.html',
+  styleUrls: ['./sidenav-toolbar.component.css']
+})
+export class SidenavToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
