69f9202991f239baf9f24a6d2ed6fa2ca6057a99
Dharani <lakshmim.velsol@gmail.com>
2019-08-22 15:38:20 +0530

    Changes

--- src/app/restaurant/tabletransfer/tabletransfer.component.ts
+++ src/app/restaurant/tabletransfer/tabletransfer.component.ts
@@ -13,9 +13,9 @@ export class TabletransferComponent implements OnInit {
   displayedColumns: string[] = ["order_id", "order_itemname","order_quantity", "order_rate","order_totalamount"];
   displayedColumns2: string[] = ["order_id", "order_itemname","order_quantity", "order_rate","order_totalamount"];
   vacantlist;occupiedlist;
-  order_id:number;order_itemname:string;order_quantity:number;
-  order_rate:number;order_totalamount:number;order_tax_amount:number;
-  kot_id:number;order_captain:string;jsRes :JsResponse;
+  order_id:any[] =[];order_itemname:any[] =[];order_quantity:any[] =[];
+  order_rate:any[] =[];order_totalamount:any[] =[];order_tax_amount:any[] =[];
+  kot_idd:any[] =[];order_captain:any[] =[];itemname_idd:any[]=[];jsRes :JsResponse;
   vacanttablen:number;occupiedtablen:number;
   constructor(private service:RestaurantService) { }
 
@@ -31,12 +31,20 @@ export class TabletransferComponent implements OnInit {
   {
     this.service.getorderitems(1,this.occupiedtablen).subscribe((data : Responce) =>
       {
-        this.order_id = data.Data[0].order_id;
-        this.order_itemname = data.Data[0].order_itemname;
-        this.order_quantity = data.Data[0].order_quantity;
-        this.order_rate = data.Data[0].order_rate;
-        this.order_totalamount = data.Data[0].order_totalamount;
-        this.dataSource = data.Data; 
+        for(let i=0;i<data.Data.length;i++)
+        {
+          this.order_id.push(data.Data[i].order_id);
+          this.order_itemname.push(data.Data[i].order_itemname);
+          this.order_quantity.push(data.Data[i].order_quantity);
+          this.order_rate.push(data.Data[i].order_rate);
+          this.order_totalamount.push(data.Data[i].order_totalamount);
+          this.order_tax_amount.push(data.Data[i].order_tax_amount);
+          this.kot_idd.push(data.Data[i].kot_id);
+          this.order_captain.push(data.Data[i].order_captain);
+          this.itemname_idd.push(data.Data[i].itemname_id);
+          this.dataSource = data.Data; 
+        }
+        
       });
   }
   onVacantChange()
@@ -55,28 +63,53 @@ export class TabletransferComponent implements OnInit {
       this.dataSource = null; 
     }
   }
+  count:number;tid:number;
+  table_t_itemname:string;table_t_rate:number;table_t_quantity:number;table_t_totalamount:number;kot_id:number;
+  table_t_captain:string;table_t_tax_amount:number;restaurent_id:number;itemname_id:number;table_defination_id:number;
   onsaveclick()
   {
-    let t:Tabletransfermodel =
-    {
-      table_t_itemname:this.order_itemname,
-      table_t_rate:this.order_rate,
-      table_t_quantity:this.order_quantity,
-      table_t_totalamount:this.order_totalamount,
-      kot_id:this.kot_id,
-      table_t_captain:this.order_captain,
-      table_t_tax_amount:this.order_tax_amount,
-      restaurent_id:1,
-      itemname_id:this.order_id,
-      table_defination_id:this.vacanttablen,
-    }
-    this.service.transferinsert(t).subscribe((data : JsResponse) => {
-
+    this.tid = this.vacanttablen;
+    this.service.transferinsert(1,this.occupiedtablen,"Running",this.tid).subscribe((data:JsResponse) =>{
       this.jsRes = data;
-      if(this.jsRes.code==200)
-          {
-            alert("Table Transferred Succesfully.!");
-          }else{ alert("Failed to transfer data");}
-     });
+        if(this.jsRes.code==200)
+        {
+          alert("Table Transfered Successfully");
+        }
+        else
+        {
+          alert("Failed to transfer table");
+        }
+    });
+    // console.log(this.dataSource2);
+    // for(let i=0;i<this.dataSource2.length;i++)
+    // {
+    //   debugger;
+    //   this.table_t_itemname=this.dataSource2[i].order_itemname,
+    //   this.table_t_rate=this.dataSource2[i].order_rate,
+    //   this.table_t_quantity=this.dataSource2[i].order_quantity,
+    //   this.table_t_totalamount=this.dataSource2[i].order_totalamount,
+    //   this.kot_id=this.dataSource2[i].kot_id,
+    //   this.table_t_captain=this.dataSource2[i].order_captain,
+    //   this.table_t_tax_amount=this.dataSource2[i].order_tax_amount,
+    //   this.restaurent_id=1,
+    //   this.itemname_id=this.dataSource2[i].itemname_id,
+    //   this.table_defination_id=this.vacanttablen,
+    //   this.service.transferinsert(this.table_t_itemname,this.table_t_rate,this.table_t_quantity,this.table_t_totalamount,this.kot_id,this.table_t_captain,this.table_t_tax_amount,this.restaurent_id,this.itemname_id,this.table_defination_id).subscribe((data : JsResponse) => {
+    //     debugger;
+    //     this.jsRes = data;
+    //     if(this.jsRes.code==200)
+    //     {
+    //       this.count = i;
+    //     }
+    //    });
+    // }
+    // if(this.count == this.count)
+    // {
+    //   alert("Table Transfered Successfully");
+    // }
+    // else
+    // {
+    //   alert("Failed to transfer table");
+    // }
   }
 }
